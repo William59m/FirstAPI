d3905bf87f6a5a3b56999d93add4fb0358ab4e17
William59m <82986293+William59m@users.noreply.github.com>
2021-09-29 23:19:14 -0300

    FirstAPI

---
+++ ContosoPizza/Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace ContosoPizza.Models
+{
+    public class Pizza
+    {
+        internal object id;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
